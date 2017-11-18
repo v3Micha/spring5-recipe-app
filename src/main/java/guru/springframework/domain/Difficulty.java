c9958e261d26740c6f2704276d623623e6b66cfa
Michael A <v-3micha@hotmail.com>
2017-11-18 14:51:50 -0500

    Add difficulty enum

---
+++ src/main/java/guru/springframework/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package guru.springframework.domain;
+
+/**
+ * Created by ev-3micha on 11/18/2017.
+ */
+public enum Difficulty {
+
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -32,6 +32,9 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
