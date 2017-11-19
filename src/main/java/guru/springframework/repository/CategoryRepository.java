ba548e8ac8407649596cd9eab4c685c68bb9496d
Michael A <v-3micha@hotmail.com>
2017-11-18 20:36:11 -0500

    Add query methods

--- src/main/java/guru/springframework/repository/CategoryRepository.java
+++ src/main/java/guru/springframework/repository/CategoryRepository.java
@@ -3,8 +3,12 @@ package guru.springframework.repository;
 import guru.springframework.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Created by ev-3micha on 11/18/2017.
  */
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/guru/springframework/repository/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repository/UnitOfMeasureRepository.java
@@ -3,8 +3,12 @@ package guru.springframework.repository;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Created by ev-3micha on 11/18/2017.
  */
 public interface UnitOfMeasureRepository extends JpaRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
