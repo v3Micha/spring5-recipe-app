53ca8396c13b401bc7096c99a0014af6389e1795
Michael A <v-3micha@hotmail.com>
2017-11-18 20:11:27 -0500

    Database initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO CATEGORY (description) VALUES ('American');
+INSERT INTO CATEGORY (description) VALUES ('Italian');
+INSERT INTO CATEGORY (description) VALUES ('Mexican');
+INSERT INTO CATEGORY (description) VALUES ('Fast Food');
+INSERT INTO CATEGORY (description) VALUES ('Chinese');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Teaspoon');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Tablespoon');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Cup');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Pinch');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Dash');
